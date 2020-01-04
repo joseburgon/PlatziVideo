6adc11b432674def3d6a61cce347504fb2147fd4
José Burgón <joseburgon9@gmail.com>
2020-01-04 17:22:59 -0500

    Adding gravatar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,44 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import gravatar from '../utils/gravatar';
 import '../assets/styles/components/Header.scss';
 import logo from '../assets/static/logo-platzi-video-BW2.png';
 import userIcon from '../assets/static/user-icon.png';
 
-const Header = () => (
-  <header className="header">
-    <Link to='/'>
-      <img className="header__img" src={logo} alt="Logo Platzi Video" />
-    </Link>
-    <div className="header__menu">
-      <div className="header__menu--profile">
-        <img src={userIcon} alt="user-icon" />
-        <p>Pefil</p>
-      </div>
-      <ul>
-        <li><a href="/">Cuenta</a></li>
-        <li>
-          <Link to='/login'>
-            Iniciar Sesión
+const Header = props => {
+  const { user } = props;
+  const hasUser = Object.keys(user).length > 0;
+  return (
+    <header className="header">
+      <Link to='/'>
+        <img className="header__img" src={logo} alt="Logo Platzi Video" />
+      </Link>
+      <div className="header__menu">
+        <div className="header__menu--profile">
+          {hasUser ?
+            <img src={gravatar(user.email)} alt={user.email} /> :
+            <img src={userIcon} alt="user-icon" />
+          }
+          <p>Pefil</p>
+        </div>
+        <ul>
+          <li><a href="/">Cuenta</a></li>
+          <li>
+            <Link to='/login'>
+              Iniciar Sesión
           </Link>
-        </li>
-      </ul>
-    </div>
-  </header>
-);
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+}
 
-export default Header;
+const mapStateToProps = state => {
+  return {
+    user: state.user
+  };
+};
+
+export default connect(mapStateToProps, null)(Header);
