53bae7b1d3f558e100b4e39c171486a2fd0f6cf7
José Burgón <joseburgon9@gmail.com>
2020-01-04 16:44:13 -0500

    Working with redux

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,38 +1,40 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import ContentWrap from '../components/ContentWrap';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
-import useInitialState from '../hooks/useInitialState';
 import '../assets/styles/Home.scss';
 
-const API = 'http://localhost:3000/initialState/';
-
-const Home = () => {
-  const initialState = useInitialState(API);
-  return initialState.length === 0 ? <h1>Loading...</h1> : (
+const Home = ({ myList, trends, originals }) => {
+  return (
     <>
       <ContentWrap>
         <Search />
-
-        { initialState.mylist.length > 0 && (
+        {myList.length > 0 && (
           <Categories title='Mi Lista'>
             <Carousel>
-              {initialState.trends.map((item) => <CarouselItem key={item.id} {...item} />)}
+              {myList.map((item) =>
+                <CarouselItem
+                key={item.id}
+                {...item}
+                isList
+                />
+              )}
             </Carousel>
           </Categories>
         )}
 
         <Categories title='Tendencias'>
           <Carousel>
-            {initialState.trends.map((item) => <CarouselItem key={item.id} {...item} />)}
+            {trends.map((item) => <CarouselItem key={item.id} {...item} />)}
           </Carousel>
         </Categories>
 
         <Categories title='Originales de PlatziVideo'>
           <Carousel>
-            {initialState.originals.map((item) => <CarouselItem key={item.id} {...item} />)}
+            {originals.map((item) => <CarouselItem key={item.id} {...item} />)}
           </Carousel>
         </Categories>
       </ContentWrap>
@@ -40,4 +42,12 @@ const Home = () => {
   );
 };
 
-export default Home;
+const mapStateToProps = state => {
+  return {
+    myList: state.myList,
+    trends: state.trends,
+    originals: state.originals
+  };
+};
+
+export default connect(mapStateToProps, null)(Home);
--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,43 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { loginRequest } from '../actions';
 import '../assets/styles/components/Login.scss';
 import googleIcon from '../assets/static/google-icon.png';
 import twitterIcon from '../assets/static/twitter-icon.png';
 
-const Login = () => (
-  <section className='login'>
-    <section className='login__container'>
-      <h2 tabIndex='0'>Inicio de Sesión</h2>
-      <form action='' className='login__container--form'>
-        <input aria-label='correo' type='text' className='input' placeholder='Correo' />
-        <input aria-label='password ' type='password' className='input' placeholder='Contraseña' />
-        <button className='button' type='button'>Iniciar Sesión</button>
-        <div className='login__container--remember-me'>
-          <label htmlFor='cbox1'>
-            <input type='checkbox' name='remember' id='cbox1' value='checkbox' />
-            Recuérdame
-          </label>
-          <a href='#'>Olvidé mi contraseña</a>
-        </div>
-      </form>
-      <section className='login__container--social-media'>
-        <div>
-          <img src={googleIcon} alt='Google' />
-          Inicia sesión con Google
-        </div>
-        <div>
-          <img src={twitterIcon} alt='Google' />
-          Inicia sesión con Twitter
-        </div>
-      </section>
-      <p className='login__container--register'>
-        ¿No tienes ninguna cuenta?
-        <Link to='/register'>
-          Regístrate
-        </Link>
-      </p>
-    </section>
-  </section>
-);
+const Login = props => {
+  const [form, setValues] = useState({
+    email: ''
+  });
 
-export default Login;
+  const handleInput = event => {
+    setValues({
+      ...form,
+      [event.target.name]: event.target.value
+    })
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    props.loginRequest(form);
+    props.history.push('/');
+  }
+
+  return (
+    <section className='login'>
+      <section className='login__container'>
+        <h2 tabIndex='0'>Inicio de Sesión</h2>
+        <form className='login__container--form' onSubmit={handleSubmit}>
+          <input
+            name='email'
+            type='text'
+            className='input'
+            placeholder='Correo'
+            onChange={handleInput}
+          />
+          <input
+            name='password '
+            type='password'
+            className='input'
+            placeholder='Contraseña'
+            onChange={handleInput}
+          />
+          <button className='button' type='submit'>Iniciar Sesión</button>
+          <div className='login__container--remember-me'>
+            <label htmlFor='cbox1'>
+              <input
+                type='checkbox'
+                name='remember'
+                id='cbox1'
+                value='checkbox'
+              />
+              Recuérdame
+          </label>
+            <a href='#'>Olvidé mi contraseña</a>
+          </div>
+        </form>
+        <section className='login__container--social-media'>
+          <div>
+            <img src={googleIcon} alt='Google' />
+            Inicia sesión con Google
+        </div>
+          <div>
+            <img src={twitterIcon} alt='Google' />
+            Inicia sesión con Twitter
+        </div>
+        </section>
+        <p className='login__container--register'>
+          ¿No tienes ninguna cuenta?
+        <Link to='/register'>
+            Regístrate
+        </Link>
+        </p>
+      </section>
+    </section>
+  );
+}
+
+const mapDispatchToProps = {
+  loginRequest
+};
+
+export default connect(null, mapDispatchToProps)(Login);
