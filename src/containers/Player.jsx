b747211802b3863c4da7ac92b74fcbac83f44a76
José Burgón <joseburgon9@gmail.com>
2020-01-05 13:48:26 -0500

    Adding Player

---
+++ src/containers/Player.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import { getVideoSource } from '../actions';
+import NotFound from '../containers/NotFound';
+import '../assets/styles/components/Player.scss';
+
+const Player = props => {
+  const { id } = props.match.params;
+  const [loading, changeLoading] = useState(true);
+  const hasPlaying = Object.keys(props.playing).length > 0;
+  useEffect(() => {
+    props.getVideoSource(id);
+    changeLoading(false);
+  }, []);
+  return loading ?
+    <h1>Loading...</h1> :
+    hasPlaying ? (
+      <div className='player'>
+        <video controls autoPlay>
+          <source src={props.playing.source} type='video/mp4' />
+        </video>
+        <div className="Player-back">
+          <button type='button' onClick={() => props.history.goBack()}>
+            Regresar
+        </button>
+        </div>
+      </div>
+    ) : <NotFound />;
+};
+
+const mapStateToProps = state => {
+  return {
+    playing: state.playing
+  }
+}
+
+const mapDispactchToProps = {
+  getVideoSource
+}
+
+export default connect(mapStateToProps, mapDispactchToProps)(Player);
